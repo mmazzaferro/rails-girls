59e99f74a2f51b27f23098eddc8fb057a5bfc6cb
Madelyne Mazzaferro <madelyne.mazzaferro@griffithuni.edu.au>
2018-08-18 02:43:28 +0000

    Update with comments and authentication

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '2.3.4'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+# Use devise for authenication
+gem 'devise'
 # Use Carrierwave to upload images
 gem 'carrierwave'
 # Use Puma as the app server
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
+    
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
@@ -58,4 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
